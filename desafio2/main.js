c40cb48a2ad4acd593d22283cbfa07afbaa56a7d
melpedernera <138738615+melpedernera@users.noreply.github.com>
2024-04-20 20:39:26 -0300

    desafio 2

---
+++ desafio2/main.js
@@ -0,0 +1,9 @@
+let Nombreproducto= prompt("ingresa el nombre del producto")
+let cantidadDeseada = prompt("ingresa la cantidad deseada del producto")
+let precioUnitario=prompt("ingresa el precio unitario del producto")
+
+let costototal= precioUnitario*cantidadDeseada
+ alert("el costo total es "+"$"+costototal)
+
+// console.log se utiliza para saber que se esta haciendo desde la pag
+console-log("Tu nombre es: " +" "+NombreUsuario1)
